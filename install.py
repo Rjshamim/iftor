d018d1cafc7f439110dbcf37e9f6a60df517fc9c
scammerplaybackbd <127949166+Rjshamim@users.noreply.github.com>
2024-07-25 12:39:12 -0400

    Update install.py

--- install.py
+++ install.py
@@ -4,15 +4,15 @@ run = os.system
 if str(choice) =='Y' or str(choice)=='y':
 
     run('chmod 777 autoTOR.py')
-    run('mkdir /usr/share/aut')
-    run('cp autoTOR.py /usr/share/aut/autoTOR.py')
+    run('mkdir /usr/share/iftor')
+    run('cp autoTOR.py /usr/share/iftor/autoTOR.py')
 
-    cmnd=(' #! /bin/sh \n exec python3 /usr/share/aut/autoTOR.py "$@"')
-    with open('/usr/bin/aut','w')as file:
+    cmnd=(' #! /bin/sh \n exec python3 /usr/share/iftor/autoTOR.py "$@"')
+    with open('/usr/bin/iftor','w')as file:
         file.write(cmnd)
-    run('chmod +x /usr/bin/aut & chmod +x /usr/share/aut/autoTOR.py')
-    print('''\n\ncongratulation auto Tor Ip Changer is installed successfully \nfrom now just type \x1b[6;30;42maut\x1b[0m in terminal ''')
+    run('chmod +x /usr/bin/iftor & chmod +x /usr/share/iftor/autoTOR.py')
+    print('''\n\ncongratulation IEHF auto Tor Ip Changer is installed successfully \nfrom now just type \x1b[6;30;42maut\x1b[0m in terminal ''')
 if str(choice)=='N' or str(choice)=='n':
-    run('rm -r /usr/share/aut ')
-    run('rm /usr/bin/aut ')
-    print('[!] now Auto Tor Ip changer  has been removed successfully')
+    run('rm -r /usr/share/iftor ')
+    run('rm /usr/bin/iftor ')
+    print('[!] now IEHF Auto Tor Ip changer  has been removed successfully')
